a6daf9227bfd4d593ad1485fdeb9ef7fa5b1dc9f
kumilachew-g <kumilachew1212@gmail.com>
2022-08-24 00:55:11 +0300

    Remove Unneccessary Comments

--- src/displayList.js
+++ src/displayList.js
@@ -3,9 +3,7 @@ const displayList = (inputs) => {
   inputs.forEach((item, i) => {
     const newSelector = document.createElement('li');
     newSelector.createElement = `<p>${item.name} : ${item.score} </p>`;
-    // if (i % 2 === 1) {
-    //   newSelector.style.backgroundColor = '#c0c0c0';
-    // }
+
     boardList.appendChild(newSelector);
   });
 };
